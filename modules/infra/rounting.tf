515ad7f6329a9f0b91843f64ebfc6f3bc231b9f0
cloud_user <egires@hotmail.com>
2023-08-11 20:54:52 -0400

    all work

--- modules/infra/rounting.tf
+++ modules/infra/rounting.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+region = "us-east-1"
+alias = "us-east"
+}
+
 resource "aws_cloudfront_cache_policy" "demo-cloudfront_cache_policy" {
   name        = "demo_cloudfront_cache_policy"
   min_ttl     = 10
@@ -23,54 +28,57 @@ resource "aws_cloudfront_cache_policy" "demo-cloudfront_cache_policy" {
   }
 }
 
-# resource "aws_cloudfront_distribution" "demo_cloudfront" {
-#   origin {
-#     domain_name              = aws_lb.demo-alb.dns_name
-#     # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-#     origin_id                = aws_lb.demo-alb.name
-#     custom_origin_config {
-#         http_port= "80"
-#         https_port="443"
-#         origin_protocol_policy = "match-viewer"
-#         origin_ssl_protocols = ["TLSv1.2"]
-#     }
+resource "aws_cloudfront_distribution" "demo_cloudfront" {
+  origin {
+    domain_name              = aws_lb.demo-alb.dns_name
+    # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = aws_lb.demo-alb.name
+    custom_origin_config {
+        http_port= "80"
+        https_port="443"
+        origin_protocol_policy = "match-viewer"
+        origin_ssl_protocols = ["TLSv1.2"]
+    }
 
-#   }
+  }
 
-#   enabled             = true
-# #   default_root_object = "index.html"
+  enabled             = true
+#   default_root_object = "index.html"
 
-#   aliases = ["numerix-md.com"]
+  aliases = ["numerix-md.com"]
 
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = aws_lb.demo-alb.name
-#     cache_policy_id = aws_cloudfront_cache_policy.demo-cloudfront_cache_policy.id
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_lb.demo-alb.name
+    cache_policy_id = aws_cloudfront_cache_policy.demo-cloudfront_cache_policy.id
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#   price_class = "PriceClass_100"
+  price_class = "PriceClass_100"
 
-#   tags = {
-#     Environment = "demo-distribution"
-#   }
+  tags = {
+    Environment = "demo-distribution"
+  }
 
-#   restrictions {
-#     geo_restriction{
-#         locations = []
-#         restriction_type = "none"
-#     }
-#   }
+  restrictions {
+    geo_restriction{
+        locations = []
+        restriction_type = "none"
+    }
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#     acm_certificate_arn = aws_acm_certificate.demo-tls-cert.id 
-#   }
-# }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    # iam_certificate_id = aws_acm_certificate.demo-tls-cert.id 
+    acm_certificate_arn = aws_acm_certificate.demo-tls-cert.id 
+    minimum_protocol_version= "TLSv1_2016"
+    ssl_support_method = "sni-only"
+  }
+}
 
 resource "aws_acm_certificate" "demo-tls-cert" {
   domain_name       = "numerix-md.com"
@@ -83,4 +91,5 @@ resource "aws_acm_certificate" "demo-tls-cert" {
   lifecycle {
     create_before_destroy = true
   }
+  provider = aws.us-east
 }
